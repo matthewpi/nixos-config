61b1b46439868173f15e0cc2f61941fab7a0d1dd
Matthew Penner <me@matthewp.io>
2023-05-30 10:20:42 -0600

    Revert "packages: add override for `jetbrains.goland`"
    This reverts commit 0cf09a2685b19bfd1d642cecc16ba0e0320b5fb2.
    
    No longer necessary since https://github.com/NixOS/nixpkgs/pull/234267
    has been merged.
    
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/default.nix
+++ packages/default.nix
@@ -5,9 +5,6 @@
     ...
   }: {
     packages = let
-      # TODO: remove once https://github.com/NixOS/nixpkgs/pull/234267 is merged
-      jetbrains = pkgs.callPackage ./overrides/jetbrains.nix {};
-
       xanmodKernels = pkgs.callPackage ./overrides/xanmod-kernels.nix {
         kernelPatches = [
           pkgs.linuxKernel.kernelPatches.bridge_stp_helper
@@ -20,17 +17,16 @@
 
       fast-syntax-highlighting = pkgs.callPackage ./fast-syntax-highlighting.nix {};
 
-      # TODO: remove once https://github.com/NixOS/nixpkgs/pull/234267 is merged
-      jetbrains-goland = jetbrains.goland;
+      xwaylandvideobridge = pkgs.libsForQt5.callPackage ./xwaylandvideobridge.nix {};
 
       # linuxPackages_xanmod = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor xanmodKernels.lts);
       linux_xanmod = xanmodKernels.lts;
+
       # linuxPackages_xanmod_stable = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor xanmodKernels.main);
       linux_xanmod_stable = xanmodKernels.main;
+
       # linuxPackages_xanmod_latest = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor xanmodKernels.main);
       linux_xanmod_latest = xanmodKernels.main;
-
-      xwaylandvideobridge = pkgs.libsForQt5.callPackage ./xwaylandvideobridge.nix {};
     };
 
     overlayAttrs = {
@@ -39,9 +35,8 @@
         catppuccin-k9s
         catppuccin-plymouth
         fast-syntax-highlighting
-        jetbrains-goland # TODO: remove once https://github.com/NixOS/nixpkgs/pull/234267 is merged
-        linux_xanmod_latest
         xwaylandvideobridge
+        linux_xanmod_latest
         ;
     };
   };
