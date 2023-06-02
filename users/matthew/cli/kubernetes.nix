44400a253cd58fea46878fceb27b96c6e2f36e28
Matthew Penner <me@matthewp.io>
2023-06-02 11:53:34 -0600

    users(matthew): add `kubelogin-oidc` to path
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/kubernetes.nix
+++ users/matthew/cli/kubernetes.nix
@@ -3,9 +3,11 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [kubernetes kubernetes-helm];
+  home.packages = with pkgs; [kubernetes kubernetes-helm kubelogin-oidc];
 
+  # Enable k9s.
   programs.k9s.enable = true;
+
   # Enable the catppuccin theme for k9s
   xdg.configFile."k9s/skin.yml".source = "${pkgs.catppuccin-k9s}/${flavour}.yaml";
 }
