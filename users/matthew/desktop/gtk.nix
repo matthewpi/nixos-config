d5ced299f240617d4b42d1cd9ed012ae8dd74fa0
Matthew Penner <me@matthewp.io>
2023-07-20 23:27:09 -0600

    users(matthew): dconf updates, add tailscale-status gnome extension
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/gtk.nix
+++ users/matthew/desktop/gtk.nix
@@ -28,7 +28,153 @@
     };
   };
 
-  dconf.settings = {
+  dconf.settings = let
+    geolocationTupleType = lib.hm.gvariant.type.tupleOf [lib.hm.gvariant.type.double lib.hm.gvariant.type.double];
+
+    mkPrimitive = t: v: {
+      _type = "gvariant";
+      type = t;
+      value = v;
+      __toString = self: "@${self.type} ${toString self.value}";
+    };
+
+    mkGeolocationTuple = elems:
+      mkPrimitive geolocationTupleType elems
+      // {
+        __toString = self: "@${self.type} (${lib.concatMapStringsSep "," toString self.value})";
+      };
+
+    mkGeolocation = {
+      somenumber ? 2,
+      name,
+      code,
+      somebool ? true,
+      coordinateX1 ? null,
+      coordinateX2 ? null,
+      coordinateY1 ? null,
+      coordinateY2 ? null,
+    }:
+      lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+        (lib.hm.gvariant.mkUint32 somenumber)
+        (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+          name
+          code
+          (lib.hm.gvariant.mkBoolean somebool)
+          (
+            if (coordinateX1 != null && coordinateX2 != null)
+            then
+              (lib.hm.gvariant.mkArray geolocationTupleType [
+                (mkGeolocationTuple [
+                  (lib.hm.gvariant.mkDouble coordinateX1)
+                  (lib.hm.gvariant.mkDouble coordinateX2)
+                ])
+              ])
+            else (lib.hm.gvariant.mkEmptyArray geolocationTupleType)
+          )
+          (
+            if (coordinateY1 != null && coordinateY2 != null)
+            then
+              (lib.hm.gvariant.mkArray geolocationTupleType [
+                (mkGeolocationTuple [
+                  (lib.hm.gvariant.mkDouble coordinateY1)
+                  (lib.hm.gvariant.mkDouble coordinateY2)
+                ])
+              ])
+            else (lib.hm.gvariant.mkEmptyArray geolocationTupleType)
+          )
+        ]))
+      ]);
+
+    calgary = mkGeolocation {
+      name = "Calgary";
+      code = "CYYC";
+      coordinateX1 = 0.89215414179553232;
+      coordinateX2 = -1.9899662412999655;
+      coordinateY1 = 0.89157235374267252;
+      coordinateY2 = -1.9911297824991001;
+    };
+
+    honolulu = mkGeolocation {
+      name = "Honolulu";
+      code = "PHNL";
+      coordinateX1 = 0.37223509621909062;
+      coordinateX2 = -2.7566263578617853;
+      coordinateY1 = 0.37187632633805073;
+      coordinateY2 = -2.7551476625596174;
+    };
+
+    vancouver = mkGeolocation {
+      name = "Vancouver";
+      code = "CYVR";
+      coordinateX1 = 0.85841109795478021;
+      coordinateX2 = -2.1496638678574467;
+      coordinateY1 = 0.85957465660720722;
+      coordinateY2 = -2.1490820798045869;
+    };
+
+    ottawa = mkGeolocation {
+      name = "Ottawa";
+      code = "CYOW";
+      coordinateX1 = 0.79092504517986117;
+      coordinateX2 = -1.3206324731601402;
+      coordinateY1 = 0.7926703744318554;
+      coordinateY2 = -1.3212142437597076;
+    };
+
+    utc = mkGeolocation {
+      name = "Coordinated Universal Time (UTC)";
+      code = "@UTC";
+      somebool = false;
+    };
+
+    sydney = mkGeolocation {
+      name = "Sydney";
+      code = "YSSY";
+      coordinateX1 = -0.59253928105207498;
+      coordinateX2 = 2.6386469349889961;
+      coordinateY1 = -0.59137572239964786;
+      coordinateY2 = 2.6392287230418559;
+    };
+  in {
+    "dev/deedles/Trayscale" = {
+      tray-icon = false;
+    };
+
+    "org/gnome/Weather" = {
+      locations = lib.hm.gvariant.mkArray lib.hm.gvariant.type.variant [calgary];
+    };
+
+    "org/gnome/clocks" = let
+      locationType = lib.hm.gvariant.type.dictionaryEntryOf [lib.hm.gvariant.type.string lib.hm.gvariant.type.variant];
+    in {
+      world-clocks = lib.hm.gvariant.mkArray (lib.hm.gvariant.type.arrayOf locationType) [
+        (lib.hm.gvariant.mkArray locationType [
+          (lib.hm.gvariant.mkDictionaryEntry ["location" honolulu])
+          (lib.hm.gvariant.mkDictionaryEntry ["location" vancouver])
+          (lib.hm.gvariant.mkDictionaryEntry ["location" calgary])
+          (lib.hm.gvariant.mkDictionaryEntry ["location" ottawa])
+          (lib.hm.gvariant.mkDictionaryEntry ["location" utc])
+          (lib.hm.gvariant.mkDictionaryEntry ["location" sydney])
+        ])
+      ];
+    };
+
+    "org/gnome/shell/weather" = {
+      automatic-location = true;
+      locations = lib.hm.gvariant.mkArray lib.hm.gvariant.type.variant [calgary];
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = lib.hm.gvariant.mkArray lib.hm.gvariant.type.variant [
+        honolulu
+        vancouver
+        calgary
+        ottawa
+        utc
+        sydney
+      ];
+    };
+
     "org/gnome/desktop/background" = {
       picture-options = "zoom";
 
@@ -94,6 +240,7 @@
 
     "org/gnome/settings-daemon/plugins/power" = {
       sleep-inactive-ac-timeout = 1800;
+      # sleep-inactive-ac-type = "nothing";
     };
 
     "org/gnome/shell" = {
@@ -105,15 +252,13 @@
         "dash-to-panel@jderose9.github.com"
         "hide-minimized@danigm.net"
         "just-perfection-desktop@just-perfection"
-        "remove-alt-tab-delay@daase.net"
-        "Vitals@CoreCoding.com"
+        "tailscale-status@maxgallup.github.com"
       ];
 
       favorite-apps = [
         "org.gnome.Nautilus.desktop"
         "firefox.desktop"
         "discord-canary.desktop"
-        "spotify.desktop"
         "com.raggesilver.BlackBox.desktop"
         "codium.desktop"
         "slack.desktop"
@@ -147,13 +292,13 @@
     };
   };
 
-  home.packages = with pkgs; [
-    gnomeExtensions.appindicator
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.dash-to-panel
-    gnomeExtensions.hide-minimized
-    gnomeExtensions.just-perfection
-    gnomeExtensions.remove-alttab-delay-v2
+  home.packages = with pkgs.gnomeExtensions; [
+    appindicator
+    blur-my-shell
+    dash-to-panel
+    hide-minimized
+    just-perfection
+    tailscale-status
   ];
 
   # Monitor settings
