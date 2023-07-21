e2a7b6a3a4301dcb31e5fd98f298afddfda5775d
Matthew Penner <me@matthewp.io>
2023-07-20 23:22:33 -0600

    packages: add override for `blackbox-terminal`
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/default.nix
+++ packages/default.nix
@@ -15,6 +15,8 @@
       _1password-gui = pkgs.callPackage ./1password-gui/default.nix {};
       _1password-gui-beta = pkgs.callPackage ./1password-gui/default.nix {channel = "beta";};
 
+      blackbox-terminal = pkgs.callPackage ./overrides/blackbox-terminal.nix {};
+
       catppuccin-k9s = pkgs.callPackage ./catppuccin/k9s.nix {};
       catppuccin-plymouth = pkgs.callPackage ./catppuccin/plymouth.nix {};
 
@@ -32,6 +34,7 @@
         (config.packages)
         _1password-gui
         _1password-gui-beta
+        blackbox-terminal
         catppuccin-k9s
         catppuccin-plymouth
         fast-syntax-highlighting
