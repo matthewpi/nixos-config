b50a2b5d8320f36c8d2f4415c0b25622b9c3296c
Matthew Penner <me@matthewp.io>
2023-07-26 13:45:47 -0600

    users(matthew): remove `kubelogin-oidc` package
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/kubernetes.nix
+++ users/matthew/cli/kubernetes.nix
@@ -4,14 +4,14 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [kubectl kubernetes-helm kubelogin-oidc];
+  home.packages = with pkgs; [kubectl kubernetes-helm];
 
-  # Change the kubeconfig location
+  # Change the default kubeconfig location.
   home.sessionVariables.KUBECONFIG = "${config.home.homeDirectory}/.config/kubernetes/config.yaml";
 
   # Enable k9s.
   programs.k9s.enable = true;
 
-  # Enable the catppuccin theme for k9s
+  # Enable the catppuccin theme for k9s.
   xdg.configFile."k9s/skin.yml".source = "${pkgs.catppuccin-k9s}/${flavour}.yaml";
 }
