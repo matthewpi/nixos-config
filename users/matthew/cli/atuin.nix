dbd36059f044d863ad48e5939e4140da4350418f
Matthew Penner <me@matthewp.io>
2023-07-26 13:45:18 -0600

    users(matthew): improve atuin configuration
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/atuin.nix
+++ users/matthew/cli/atuin.nix
@@ -1,5 +1,16 @@
 {
   programs.atuin = {
     enable = true;
+    enableBashIntegration = false;
+    enableFishIntegration = false;
+    enableNushellIntegration = false;
+    enableZshIntegration = true;
+
+    settings = {
+      auto_sync = false;
+      update_check = false;
+      search_mode = "prefix";
+      filter_mode = "host";
+    };
   };
 }
