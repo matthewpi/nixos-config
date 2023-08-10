44dbd5dcd8580b0a489047932a2fd2f3c32358c6
Matthew Penner <me@matthewp.io>
2023-08-09 15:47:10 -1000

    users(matthew): fix 1password IdentityAgent path on darwin
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/ssh.nix
+++ users/matthew/cli/ssh.nix
@@ -8,7 +8,7 @@
           # Configure the 1Password SSH agent
           IdentityAgent =
             if pkgs.stdenv.isDarwin
-            then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+            then "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
             else "~/.1password/agent.sock";
           # Change the known_hosts location to outside .ssh as that directory is read-only when
           # impermanence is enabled
