d29ed7ea7851db7f5bccdad7bc9c450f9c311bd9
Matthew Penner <me@matthewp.io>
2023-11-30 00:30:02 -0700

    packages(cider2): add flags to fix broken rendering
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/cider2/linux.nix
+++ packages/cider2/linux.nix
@@ -22,7 +22,7 @@ in
 
       wrapProgram "$out/bin/${pname}" \
         --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-        --add-flags '--disable-seccomp-filter-sandbox'
+        --add-flags '--disable-seccomp-filter-sandbox --ignore-gpu-blocklist --enable-gpu-rasterization --enable-gpu --enable-features=Vulkan,UseSkiaRenderer,VaapiVideoDecoder,CanvasOopRasterization,VaapiVideoEncoder,RawDraw --disable-features=UseChromeOSDirectVideoDecoder --enable-zero-copy --enable-oop-rasterization --enable-raw-draw --enable-accelerated-mjpeg-decode --enable-accelerated-video --enable-native-gpu-memory-buffers'
 
       install -m 444 -D '${extracted}/usr/share/icons/hicolor/512x512/apps/${pname}.png' \
         "$out/share/icons/hicolor/512x512/apps/${pname}.png"
