58c207160c41eb7f24e10361b163d7982a61b599
Matthew Penner <me@matthewp.io>
2023-12-07 12:59:52 -0700

    users(matthew): set variables to help with XDG spec
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/default.nix
+++ users/matthew/default.nix
@@ -26,7 +26,21 @@
   };
 
   home.sessionVariables = {
-    # Change GOPATH from the default ~/go location to reduce clutter.
-    GOPATH = "${config.home.homeDirectory}/.local/share/go";
+    XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
+    XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
+    XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
+    XDG_STATE_HOME = "${config.home.homeDirectory}/.local/state";
+    # XDG_CACHE_HOME = config.xdg.cacheHome;
+    # XDG_CONFIG_HOME = config.xdg.configHome;
+    # XDG_DATA_HOME = config.xdg.dataHome;
+    # XDG_STATE_HOME = config.xdg.stateHome;
+
+    # Change GNUPGHOME from the default ~/.gnupg location to follow the XDG spec.
+    GNUPGHOME = "${config.home.sessionVariables.XDG_DATA_HOME}/gnupg";
+
+    # Change GOPATH from the default ~/go location to follow the XDG spec.
+    GOPATH = "${config.home.sessionVariables.XDG_DATA_HOME}/go";
+
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.home.sessionVariables.XDG_CONFIG_HOME}/java";
   };
 }
