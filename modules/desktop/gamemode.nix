1ad90ba572cf2cc1785ac0a2e09b0788cb5f4e3a
Matthew Penner <me@matthewp.io>
2023-12-29 12:45:09 -0700

    modules(desktop): run `gamemoded` under `background.slice`
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/desktop/gamemode.nix
+++ modules/desktop/gamemode.nix
@@ -1,5 +1,7 @@
 {
+  # Enable gamemode and the gamemoded service.
   programs.gamemode.enable = true;
+  systemd.user.services.gamemoded.serviceConfig.Slice = "background.slice";
 
   boot.kernel.sysctl = {
     # SteamOS/Fedora default, can help with performance.
