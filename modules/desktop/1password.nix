3ea622d46fb7bf79a9ef456cce104f3899a5fdfb
Matthew Penner <me@matthewp.io>
2023-12-29 12:43:56 -0700

    modules(desktop): remove 1password autostart
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/desktop/1password.nix
+++ modules/desktop/1password.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   pkgs,
   ...
@@ -9,12 +8,4 @@
     enable = lib.mkDefault true;
     package = lib.mkDefault pkgs._1password-gui-beta;
   };
-
-  # TODO: add an option to enable/disable this behaviour?
-  environment.systemPackages = lib.mkIf config.programs._1password-gui.enable [
-    (pkgs.makeAutostartItem {
-      name = "1password";
-      package = config.programs._1password-gui.package;
-    })
-  ];
 }
