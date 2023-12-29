c16a0b9059f44811f1b82442515679f7dc49487b
Matthew Penner <me@matthewp.io>
2023-12-29 12:44:25 -0700

    modules(desktop): fix pipewire config
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/desktop/pipewire.nix
+++ modules/desktop/pipewire.nix
@@ -1,9 +1,19 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  rate = 96000;
+  # TODO: add to config
+  #rates = [44100 48000 96000];
+  # Disabled due to causing audio cracks, re-visit once we figure out a solution.
+  quantum = 240; # (rate / 1000) * 2.5
+in {
   # Disable pulseaudio
   hardware.pulseaudio.enable = false;
 
   # Enable real-time kit
-  security.rtkit.enable = lib.mkDefault true;
+  security.rtkit.enable = lib.mkDefault config.services.pipewire.enable;
 
   # Enable pipewire
   services.pipewire = {
@@ -15,16 +25,18 @@
     pulse.enable = lib.mkDefault true;
   };
 
-  # TODO: this configuration is very specific to my audio setup,
-  # maybe add config options or a way to tweak this easily?
-  environment.etc."pipewire/99-custom.conf".text = ''
+  # Low-latency configuraiton
+  environment.etc."pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
     context.properties = {
-      default.clock.rate = 96000
+      default.clock.rate = ${toString rate}
       default.clock.allowed-rates = [44100 48000 96000]
 
-      default.clock.min-quantum = 96
+      #default.clock.min-quantum = ${toString quantum}
+      #default.clock.max-quantum = ${toString quantum}
+      #default.clock.quantum = ${toString quantum}
     }
-
+  '';
+  environment.etc."pipewire/pipewire-pulse.d/92-low-latency.conf".text = ''
     context.modules = [
       {
         name = "libpipewire-module-rtkit"
@@ -36,38 +48,21 @@
         }
         flags = ["ifexists" "nofail"]
       }
-
-      {
-        name = "libpipewire-module-protocol-native"
-      }
-
-      {
-        name = "libpipewire-module-client-node"
-      }
-
-      {
-        name = "libpipewire-module-adapter"
-      }
-
-      {
-        name = "libpipewire-module-metadata"
-      }
-
       {
         name = "libpipewire-module-protocol-pulse"
         args = {
-          pulse.min.req = 96/96000
-          pulse.min.quantum = 96/96000
-          pulse.min.frag = 96/96000
+          #pulse.min.req = ${toString quantum}/${toString rate}
+          #pulse.min.quantum = ${toString quantum}/${toString rate}
+          #pulse.min.frag = ${toString quantum}/${toString rate}
           server.address = ["unix:native"]
         }
       }
     ]
 
-    stream.properties = {
-      node.latecy = 96/96000
-      resample.quality = 1
-    }
+    #stream.properties = {
+    #  node.latency = ${toString quantum}/${toString rate}
+    #  resample.quality = 1
+    #}
   '';
 
   # Enable sound
