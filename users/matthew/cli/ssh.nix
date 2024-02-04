546eb12830f519089a9f458e59955611da7e14a1
Matthew Penner <me@matthewp.io>
2024-02-04 15:01:34 -0700

    users(matthew): disable 1Password SSH agent
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/ssh.nix
+++ users/matthew/cli/ssh.nix
@@ -1,15 +1,10 @@
-{pkgs, ...}: {
+{
   programs.ssh = {
     enable = true;
 
     matchBlocks = {
       "*" = {
         extraOptions = {
-          # Configure the 1Password SSH agent
-          IdentityAgent =
-            if pkgs.stdenv.isDarwin
-            then "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
-            else "~/.1password/agent.sock";
           # Change the known_hosts location to outside .ssh as that directory is read-only when
           # impermanence is enabled
           UserKnownHostsFile = "~/.cache/ssh/known_hosts";
