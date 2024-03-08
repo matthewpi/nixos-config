cfa4c7d04c8b8a6070a01e3d87f17f28439b754c
Matthew Penner <me@matthewp.io>
2024-03-08 13:45:22 -0700

    users(matthew): make npm follow xdg dir spec
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/default.nix
+++ users/matthew/cli/default.nix
@@ -25,4 +25,18 @@
 
     nix-output-monitor
   ];
+
+  home.sessionVariables.NPM_CONFIG_USERCONFIG = ''''${XDG_CONFIG_HOME}/npm/npmrc'';
+
+  xdg.configFile."npm/npmrc".text = ''
+    # NPM
+    prefix=''${XDG_DATA_HOME}/npm
+    cache=''${XDG_CACHE_HOME}/npm
+    init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+    tmp=''${XDG_RUNTIME_DIR}/npm
+
+    # PNPM
+    state-dir=''${XDG_STATE_HOME}/pnpm-state
+    store-dir=''${XDG_DATA_HOME}/pnpm-store
+  '';
 }
