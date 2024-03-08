31b019ce9610888066fa21d8d719ec45a768222b
Matthew Penner <me@matthewp.io>
2024-03-08 13:51:28 -0700

    modules(desktop): pipewire low-latency
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/desktop/pipewire.nix
+++ modules/desktop/pipewire.nix
@@ -3,6 +3,9 @@
   lib,
   ...
 }: {
+  # Enable sound
+  sound.enable = lib.mkDefault true;
+
   # Disable pulseaudio
   hardware.pulseaudio.enable = lib.mkForce false;
 
@@ -19,8 +22,47 @@
       support32Bit = lib.mkDefault true;
     };
     pulse.enable = lib.mkDefault true;
+
+    # Configure low-latency quantum values for pipewire.
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        # "default.clock.rate" = 96000;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 512;
+      };
+    };
   };
 
-  # Enable sound
-  sound.enable = lib.mkDefault true;
+  services.udev.extraRules = ''
+    KERNEL=="rtc0", GROUP="audio"
+    KERNEL=="hpet", GROUP="audio"
+  '';
+
+  security.pam.loginLimits = [
+    {
+      domain = "@audio";
+      item = "memlock";
+      type = "-";
+      value = "unlimited";
+    }
+    {
+      domain = "@audio";
+      item = "rtprio";
+      type = "-";
+      value = "99";
+    }
+    {
+      domain = "@audio";
+      item = "nofile";
+      type = "soft";
+      value = "99999";
+    }
+    {
+      domain = "@audio";
+      item = "nofile";
+      type = "hard";
+      value = "524288";
+    }
+  ];
 }
