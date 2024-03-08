10013c093577f7bf6a251c196cefc9bb35d8b214
Matthew Penner <me@matthewp.io>
2024-03-08 12:08:38 -0700

    modules(persistence): fix NetworkManager causing activation errors
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/persistence/networkmanager.nix
+++ modules/persistence/networkmanager.nix
@@ -5,13 +5,9 @@
 }: {
   config = lib.mkIf config.networking.networkmanager.enable {
     environment.persistence."/persist" = {
-      directories = ["/etc/NetworkManager/system-connections"];
-
-      files = [
-        "/var/lib/NetworkManager/NetworkManager.state"
-        "/var/lib/NetworkManager/secret_key"
-        "/var/lib/NetworkManager/seen-bssids"
-        "/var/lib/NetworkManager/timestamps"
+      directories = [
+        "/etc/NetworkManager/system-connections"
+        "/var/lib/NetworkManager"
       ];
     };
   };
