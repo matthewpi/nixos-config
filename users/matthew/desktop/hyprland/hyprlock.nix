5309b35ac959a9ea1bc0a34793a0ae0ce034d324
Matthew Penner <me@matthewp.io>
2024-05-11 13:33:26 -0600

    users(matthew): use home-manager `hyprlock` module
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/hyprland/hyprlock.nix
+++ users/matthew/desktop/hyprland/hyprlock.nix
@@ -1,40 +1,33 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  imports = [
-    inputs.hyprlock.homeManagerModules.default
-  ];
-
+{pkgs, ...}: {
   # https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/
   programs.hyprlock = {
     enable = true;
-    package = pkgs.hyprlock;
 
-    general = {
-      disable_loading_bar = false;
-      hide_cursor = true;
-      grace = 10;
-      no_fade_in = false;
+    settings = {
+      general = {
+        disable_loading_bar = false;
+        hide_cursor = true;
+        grace = 10;
+        no_fade_in = false;
+      };
+
+      background = [
+        {
+          monitor = ""; # All monitors
+          path = "${pkgs.catppuccin-wallpapers}/nix-black-4k.png";
+          # Blur the background
+          blur_passes = 2;
+          # Dim the background so we can tell the difference when the system is locked.
+          brightness = 0.5;
+        }
+      ];
+
+      label = [
+        {
+          monitor = "";
+          font_family = "Inter";
+        }
+      ];
     };
-
-    backgrounds = [
-      {
-        monitor = ""; # All monitors
-        path = "${pkgs.catppuccin-wallpapers}/nix-black-4k.png";
-        # Blur the background
-        blur_passes = 2;
-        # Dim the background so we can tell the difference when the system is locked.
-        brightness = 0.5;
-      }
-    ];
-
-    labels = [
-      {
-        monitor = "";
-        font_family = "Inter";
-      }
-    ];
   };
 }
