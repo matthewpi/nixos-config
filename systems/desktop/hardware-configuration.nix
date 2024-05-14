230687270181bb89e5aaaa9cb8e34b800ed8c14c
Matthew Penner <me@matthewp.io>
2024-05-14 12:14:57 -0600

    systems(desktop): change boot loader timeout to 3 seconds (from 15)
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- systems/desktop/hardware-configuration.nix
+++ systems/desktop/hardware-configuration.nix
@@ -13,8 +13,8 @@ in {
   boot.initrd.availableKernelModules = ["ahci" "nvme" "usbhid" "xhci_pci"];
   boot.initrd.supportedFilesystems = ["btrfs" "ntfs"];
 
-  # Change the boot loader timeout to 15 seconds.
-  boot.loader.timeout = 15;
+  # Change the boot loader timeout to 3 seconds.
+  boot.loader.timeout = 3;
 
   # Use systemd stage 1
   boot.initrd.systemd.enable = true;
