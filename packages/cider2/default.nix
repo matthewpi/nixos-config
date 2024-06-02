9a978083e2a994054ee222b7eabab8f05fe25f45
Matthew Penner <me@matthewp.io>
2024-06-02 14:02:29 -0600

    cider2: 2.4.0 -> 2.4.1
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/cider2/default.nix
+++ packages/cider2/default.nix
@@ -5,22 +5,22 @@
   stdenv,
 }: let
   pname = "cider";
-  version = "2.4.0";
+  version = "2.4.1";
 
   # For anyone wondering, these files are locked behind firewall rules.
   # If you want Cider2, go buy it yourself.
   sources = {
     aarch64-darwin = {
       url = "https://r2.matthewp.io/cider/${version}/Cider-${version}-aarch64.dmg";
-      hash = "sha256-PoScpJuwAaNbLkcj9mKZF29IYCnQzP9UJHkGZm2kUR0=";
+      hash = "sha256-wpVPebFiruUD9U/bHHN3CMMWIvjzO2K3tLYP9KGPNPk=";
     };
     x86_64-darwin = {
       url = "https://r2.matthewp.io/cider/${version}/Cider-${version}-x86_64.dmg";
-      hash = "sha256-BjNwF7s2uVJ/uiXlae+zU8SbQlYlezwYeAkkoUDkFKc=";
+      hash = "sha256-mGozZoNiFjUQURMZIROItJbhKHNit9pyChoaYVPzqZU=";
     };
     x86_64-linux = {
       url = "https://r2.matthewp.io/cider/${version}/Cider-${version}.AppImage";
-      hash = "sha256-PSOJYaxEGahqX/7XIKkAiObqHwuAu6YI7CF/qc9DOZU=";
+      hash = "sha256-p1NH11+QvkhXrPnfsozsbmrY4ZkyVQ1wcFqLkmV+R7c=";
     };
   };
 
