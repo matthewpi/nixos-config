005e1b1caeb66ad8fa11366cdec86a5f789c1ddf
Matthew Penner <me@matthewp.io>
2024-08-17 12:15:23 -0600

    linux_xanmod_main: add pname, update kernel build config
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/kernel/xanmod-kernels.nix
+++ packages/kernel/xanmod-kernels.nix
@@ -22,6 +22,7 @@
     buildLinux (args
       // rec {
         inherit version;
+        pname = "linux-xanmod";
         modDirVersion = lib.versions.pad 3 "${version}-${suffix}";
 
         src = applyPatches {
@@ -38,6 +39,14 @@
         };
 
         structuredExtraConfig = with lib.kernel; {
+          # CPUFreq governor Performance
+          CPU_FREQ_DEFAULT_GOV_PERFORMANCE = lib.mkOverride 60 yes;
+          CPU_FREQ_DEFAULT_GOV_SCHEDUTIL = lib.mkOverride 60 no;
+
+          # Full preemption
+          PREEMPT = lib.mkOverride 60 yes;
+          PREEMPT_VOLUNTARY = lib.mkOverride 60 no;
+
           # Google's BBRv3 TCP congestion Control
           TCP_CONG_BBR = yes;
           DEFAULT_BBR = yes;
@@ -46,6 +55,14 @@
           HZ = freeform "250";
           HZ_250 = yes;
           HZ_1000 = no;
+
+          # RCU_BOOST and RCU_EXP_KTHREAD
+          RCU_EXPERT = yes;
+          RCU_FANOUT = freeform "64";
+          RCU_FANOUT_LEAF = freeform "16";
+          RCU_BOOST = yes;
+          RCU_BOOST_DELAY = freeform "0";
+          RCU_EXP_KTHREAD = yes;
 
           DRM_AMD_COLOR_STEAMDECK = yes;
         };
