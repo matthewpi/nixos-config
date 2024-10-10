838f4475a614cf3e2d11cd6574ff41451b3a62e9
Matthew Penner <me@matthewp.io>
2024-10-10 12:37:28 -0600

    linux_xanmod_main: 6.11.2 -> 6.11.3
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/kernel/xanmod-kernels.nix
+++ packages/kernel/xanmod-kernels.nix
@@ -9,8 +9,8 @@
   # NOTE: When updating these, please also take a look at the changes done to
   # kernel config in the xanmod version commit
   mainVariant = {
-    version = "6.11.2";
-    hash = "sha256-4BXPZs8lp/O/JGWFIO/J1HyOjByaqWQ9O6/jx76TIDs=";
+    version = "6.11.3";
+    hash = "sha256-Pb/7XToBFZstI1DFgWg4a2HiRuSzA9rEsMBLb6fRvYc=";
   };
 
   xanmodKernelFor = {
