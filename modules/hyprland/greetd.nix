10c0829b95eaa11638d28dd3c6173d118cbcab71
Matthew Penner <me@matthewp.io>
2024-10-20 12:28:14 -0600

    modules(hyprland): disable `fprintAuth` for greetd
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/hyprland/greetd.nix
+++ modules/hyprland/greetd.nix
@@ -20,4 +20,14 @@
       user = "greeter";
     };
   };
+
+  # Disable fprint auth for greetd.
+  #
+  # greetd is used immediately after boot to start a user session. If
+  # fingerprint auth is used, gnome-keyring won't get unlocked, requiring a
+  # password later anyways. So to avoid this, just use a password instead.
+  #
+  # A fingerprint can still be used with other services once the user session
+  # has been started.
+  security.pam.services.greetd.fprintAuth = false;
 }
