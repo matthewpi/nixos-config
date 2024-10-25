02363be85e0848c285c942d116d8f7d76a9eeff1
Matthew Penner <me@matthewp.io>
2024-10-24 20:55:53 -0600

    users(matthew): update `tree-sitter`
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/tree-sitter.nix
+++ users/matthew/cli/tree-sitter.nix
@@ -1,12 +1,8 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   xdg.configFile."tree-sitter/config.json".source = (pkgs.formats.json {}).generate "tree-sitter-config.json" {
     parser-directories = [
       pkgs.tree-sitter.grammars
-      "${config.home.homeDirectory}/code/caddyserver"
+      "/code/caddyserver"
     ];
   };
 }
