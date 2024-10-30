2c9229c6bf5a89f123e530db219b22579017a3e2
Matthew Penner <me@matthewp.io>
2024-10-29 22:31:29 -0600

    users(matthew): fix `easyeffects` service blocking due to not having proper dependencies
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/services/easyeffects-service.nix
+++ users/matthew/desktop/services/easyeffects-service.nix
@@ -4,13 +4,23 @@
   ...
 }: {
   config = lib.mkIf config.services.easyeffects.enable {
-    systemd.user.services.easyeffects.Service = {
-      Type = "dbus";
-      BusName = "com.github.wwmm.easyeffects";
-      ExecStart = lib.mkForce "${lib.getExe config.services.easyeffects.package} --gapplication-service";
-      ExecStartPost = [
-        "${lib.getExe config.services.easyeffects.package} --load-preset ${config.services.easyeffects.preset}"
-      ];
+    systemd.user.services.easyeffects = {
+      Unit = {
+        After = lib.mkForce ["pipewire.service"];
+        BindsTo = lib.mkForce ["pipewire.service"];
+        PartOf = lib.mkForce [];
+      };
+
+      Install.WantedBy = lib.mkForce ["pipewire.service"];
+
+      Service = {
+        Type = "dbus";
+        BusName = "com.github.wwmm.easyeffects";
+        ExecStart = lib.mkForce "${lib.getExe config.services.easyeffects.package} --gapplication-service";
+        ExecStartPost = [
+          "${lib.getExe config.services.easyeffects.package} --load-preset ${config.services.easyeffects.preset}"
+        ];
+      };
     };
   };
 }
