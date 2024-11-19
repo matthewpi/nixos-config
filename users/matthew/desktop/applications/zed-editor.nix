bd6983a3bc8b0b1f8b3a802ce717bed140c8c2a8
Matthew Penner <me@matthewp.io>
2024-11-19 10:19:43 -0700

    users(matthew): disable `zed-editor`, update config
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/applications/zed-editor.nix
+++ users/matthew/desktop/applications/zed-editor.nix
@@ -1,8 +1,70 @@
 {
   programs.zed-editor = {
-    enable = true;
-    extensions = [];
+    enable = false;
+
+    extensions = [
+      "assembly"
+      "bitbake"
+      "blade"
+      "capnp"
+      "catppuccin"
+      "csv"
+      "cue"
+      "d2"
+      "dockerfile"
+      "emmet"
+      "env"
+      "golangci-lint"
+      "gosum"
+      "graphql"
+      "helm"
+      "html"
+      "http"
+      "ini"
+      "jinja2"
+      "just"
+      "log"
+      "make"
+      "meson"
+      "nix"
+      "nu"
+      "php"
+      "scss"
+      "snippets"
+      "sql"
+      "terraform"
+      "toml"
+      "typst"
+      "vala"
+      "vue"
+      "xml"
+      "zig"
+    ];
+
     userKeymaps = [];
-    userSettings = {};
+
+    userSettings = {
+      auto_update = false;
+      base_keymap = "VSCode";
+
+      buffer_font_family = "MonaspiceNe Nerd Font";
+      buffer_font_size = 16;
+
+      ui_font_family = "Inter";
+      ui_font_size = 14;
+
+      load_direnv = "shell_hook";
+
+      ensure_final_newline_on_save = true;
+
+      features = {
+        copilot = false;
+      };
+
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+    };
   };
 }
