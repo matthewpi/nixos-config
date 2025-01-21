b883619f22e22a94218b015958ce0f762be47e82
Matthew Penner <me@matthewp.io>
2025-01-21 11:20:32 -0700

    modules(ags): handle adding/removing monitors correctly
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/ags/app.ts
+++ modules/ags/app.ts
@@ -1,4 +1,5 @@
 import { App, Gdk, Gtk } from 'astal/gtk4';
+import Gio from 'gi://Gio';
 
 import { Launcher } from './components/apps';
 import { Bar } from './components/bar';
@@ -6,20 +7,57 @@ import { NotificationsWindow } from './components/notifications';
 import style from './style.scss';
 
 function main() {
-	const bars = new Map<Gdk.Monitor, Gtk.Widget>();
-
-	for (const monitor of App.get_monitors()) {
-		bars.set(monitor, Bar(monitor));
+	const display = Gdk.Display.get_default() ?? undefined;
+	if (display === undefined) {
+		return;
 	}
 
-	// App.connect('monitor-added', (_, monitor) => {
-	// 	bars.set(monitor, Bar(monitor));
-	// });
+	// @ts-expect-error go away
+	const monitors: Gio.ListModel<Gdk.Monitor> = display.get_monitors();
 
-	// App.connect('monitor-removed', (_, monitor) => {
-	// 	bars.get(monitor)?.destroy();
-	// 	bars.delete(monitor);
-	// });
+	const bars = new Map<string, Gtk.Widget>();
+
+	// Add a Bar for every monitor.
+	{
+		let monitor: Gdk.Monitor | null;
+		for (let i = 0; (monitor = monitors.get_item(i)) !== null; i++) {
+			console.log(monitor.connector, monitor);
+			bars.set(monitor.connector, Bar(monitor));
+		}
+	}
+
+	// Watch for changes to the monitors.
+	monitors.connect('items-changed', async (v: Gio.ListModel<Gdk.Monitor>) => {
+		// TODO: without this, it seems that a newly added monitor's `connector`
+		// will be `null`.
+		await new Promise(r => setTimeout(r, 100));
+
+		const monitors = new Map<string, Gdk.Monitor>();
+
+		let monitor: Gdk.Monitor | null;
+		for (let i = 0; (monitor = v.get_item(i)) !== null; i++) {
+			monitors.set(monitor.connector, monitor);
+		}
+
+		for (const connector of bars.keys()) {
+			const monitor = monitors.get(connector);
+			if (monitor === undefined) {
+				console.log('monitor-removed', connector);
+				// If there is no longer a monitor, delete the bar entry.
+				bars.delete(connector);
+				continue;
+			}
+
+			// If the monitor still exists, remove it from the monitors list.
+			console.log('monitor-unchanged', connector);
+			monitors.delete(connector);
+		}
+
+		for (const [connector, monitor] of monitors) {
+			console.log('monitor-added', connector);
+			bars.set(connector, Bar(monitor));
+		}
+	});
 
 	NotificationsWindow();
 
@@ -29,9 +67,4 @@ function main() {
 App.start({
 	css: style,
 	main,
-	requestHandler(request: string, res: (response: any) => void) {
-		print(request);
-
-		return res('ok');
-	},
 });
