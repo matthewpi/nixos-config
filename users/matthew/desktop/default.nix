d0f776f48bf64838295f4fe3c58a5dba39e8977e
Matthew Penner <me@matthewp.io>
2025-02-12 20:28:02 -0700

    users(matthew): install `devenv`
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/default.nix
+++ users/matthew/desktop/default.nix
@@ -9,14 +9,14 @@
     ./streamdeck.nix
   ];
 
-  # https://nix-community.github.io/home-manager/options.xhtml#opt-systemd.user.startServices
-  systemd.user.startServices = "sd-switch";
-
   home.packages = with pkgs; [
     # Install nvtop
     nvtopPackages.amd
 
     # Install kind
     kind
+
+    # Install devenv
+    devenv
   ];
 }
