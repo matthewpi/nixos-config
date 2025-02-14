4e458fbb12a9db03008b1a190ddee2c6f36ec55b
Matthew Penner <me@matthewp.io>
2025-02-14 12:37:10 -0700

    users(matthew): prevent atuin from shifting terminal lines after closing
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/atuin.nix
+++ users/matthew/cli/atuin.nix
@@ -2,11 +2,15 @@
   programs.atuin = {
     enable = true;
 
+    # daemon.enable = true;
+
     settings = {
       auto_sync = false;
       update_check = false;
       search_mode = "prefix";
       filter_mode = "host";
+
+      keys.scroll_exits = false;
     };
   };
 }
