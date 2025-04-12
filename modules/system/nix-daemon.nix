0688395af8f3636b62889b895b9217a4c6f1f240
Matthew Penner <me@matthewp.io>
2025-04-12 13:26:49 -0600

    modules(system): use `nix_2_28` by default
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/system/nix-daemon.nix
+++ modules/system/nix-daemon.nix
@@ -5,7 +5,7 @@
 }: {
   nix = {
     # Use the latest version of Nix
-    package = pkgs.nixVersions.nix_2_26;
+    package = pkgs.nixVersions.nix_2_28;
 
     # Disable nix channels since we don't use them.
     channel.enable = false;
