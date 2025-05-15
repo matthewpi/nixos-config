4ceddd20879c78f0d32a27f3942c504632abd671
Matthew Penner <me@matthewp.io>
2025-05-15 16:08:56 -0600

    users(matthew): include 1Password SSH config
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/ssh.nix
+++ users/matthew/cli/ssh.nix
@@ -2,14 +2,14 @@
   programs.ssh = {
     enable = true;
 
-    matchBlocks = {
-      "*" = {
-        extraOptions = {
-          # Change the known_hosts location to outside .ssh as that directory is read-only when
-          # impermanence is enabled
-          UserKnownHostsFile = "~/.cache/ssh/known_hosts";
-        };
-      };
+    # Include the 1Password bookmarks config, this also helps ensure we only
+    # use the proper SSH key for the host.
+    includes = ["~/.ssh/1Password/config"];
+
+    matchBlocks."*".extraOptions = {
+      # Change the known_hosts location to outside `~/.ssh` as `~/.ssh` is not
+      # persisted by impermanence.
+      UserKnownHostsFile = "~/.cache/ssh/known_hosts";
     };
   };
 }
