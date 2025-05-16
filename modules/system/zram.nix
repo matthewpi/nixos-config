742cbe3f789b5bbe46499747230fe82f7930a78e
Matthew Penner <me@matthewp.io>
2025-05-16 13:25:10 -0600

    modules(system): use NixOS module for zram instead of manual config
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/system/zram.nix
+++ modules/system/zram.nix
@@ -1,10 +1,9 @@
-{pkgs, ...}: {
-  environment.etc."systemd/zram-generator.conf".text = ''
-    [zram0]
-    zram-size = min(ram / 4, 16384)
-    compression-algorithm = zstd
-  '';
-
-  systemd.packages = with pkgs; [zram-generator];
-  systemd.services."systemd-zram-setup@".path = with pkgs; [util-linux];
+{
+  services.zram-generator = {
+    enable = true;
+    settings.zram0 = {
+      zram-size = "min(ram / 4, 16384)";
+      compression-algorithm = "zstd";
+    };
+  };
 }
