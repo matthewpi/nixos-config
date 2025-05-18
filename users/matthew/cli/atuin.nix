04db82f133983157745ae98fbabbc05e6a73c80e
Matthew Penner <me@matthewp.io>
2025-05-18 15:40:39 -0600

    users(matthew): enable atuin sync to self-hosted server
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/atuin.nix
+++ users/matthew/cli/atuin.nix
@@ -5,10 +5,13 @@
     # daemon.enable = true;
 
     settings = {
-      auto_sync = false;
       update_check = false;
       search_mode = "prefix";
       filter_mode = "host";
+
+      auto_sync = true;
+      sync_address = "https://atuin.blahaj.systems";
+      sync_frequency = "15m";
 
       inline_height = 0;
     };
