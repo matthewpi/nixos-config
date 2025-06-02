3f55a98b22972ded2bd299064da91e264941d840
Matthew Penner <me@matthewp.io>
2025-06-02 11:28:06 -0600

    modules(desktop): disable amdgpu overdrive
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/desktop/corectrl.nix
+++ modules/desktop/corectrl.nix
@@ -1,9 +1,5 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{lib, ...}: {
   programs.corectrl.enable = lib.mkDefault true;
   # https://gitlab.com/corectrl/corectrl/-/wikis/Setup#full-amd-gpu-controls
-  hardware.amdgpu.overdrive.enable = lib.mkDefault config.programs.corectrl.enable;
+  # hardware.amdgpu.overdrive.enable = lib.mkDefault config.programs.corectrl.enable;
 }
