2d1fb7a1fdc18773940fc2a394252c801f7af05b
Matthew Penner <me@matthewp.io>
2025-06-28 13:38:32 -0600

    nix: update flake.lock
    Flake lock file updates:
    
    • Updated input 'determinate-nixd-x86_64-linux':
        'https://install.determinate.systems/determinate-nixd/tag/v3.6.7/x86_64-linux?narHash=sha256-mcBD5mliHui0BEXg64cgQbLSKMWJBydHvDCleBDnNsU%3D'
      → 'https://install.determinate.systems/determinate-nixd/tag/v3.6.8/x86_64-linux?narHash=sha256-jHpbawa%2BAsMNL0uHuSIQ0Loy4ddEAwBmoxqIW7YKzBA%3D'
    • Updated input 'nix':
        'github:DeterminateSystems/nix-src/c224b8359c978d378e3cea3a6782f2ee796ddb13?narHash=sha256-3JtVZilOddowCXzGEy/CprNF8eMZkKCweqe07KBhE3Y%3D' (2025-06-24)
      → 'github:DeterminateSystems/nix-src/3a8bd3b108657ab95d62582c4f590624d3fe761c?narHash=sha256-mvC0XyabdgMEYRpFwTrfadYZnbH8mDO/Po7dLnGAZGA%3D' (2025-06-25)

--- flake.nix
+++ flake.nix
@@ -258,7 +258,7 @@
     };
 
     nix = {
-      url = "github:DeterminateSystems/nix-src/v3.6.7";
+      url = "github:DeterminateSystems/nix-src/v3.6.8";
       inputs = {
         flake-parts.follows = "flake-parts";
         git-hooks-nix.follows = "";
@@ -270,7 +270,7 @@
 
     # NOTE: unfree
     determinate-nixd-x86_64-linux = {
-      url = "https://install.determinate.systems/determinate-nixd/tag/v3.6.7/x86_64-linux";
+      url = "https://install.determinate.systems/determinate-nixd/tag/v3.6.8/x86_64-linux";
       flake = false;
     };
   };
