7f22246178a5c19e0b83b4e9fba23d23adcef66b
Matthew Penner <me@matthewp.io>
2025-06-29 14:03:21 -0600

    freelens-k8s-proxy: 1.3.2 -> 1.3.4
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/freelens-k8s-proxy/package.nix
+++ packages/freelens-k8s-proxy/package.nix
@@ -5,16 +5,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "freelens-k8s-proxy";
-  version = "1.3.2";
+  version = "1.3.4";
 
   src = fetchFromGitHub {
     owner = "freelensapp";
     repo = "freelens-k8s-proxy";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ou2KNerwhtveYqVRweSR7nJkKUzsYCA2yedim5Q+Qy4=";
+    hash = "sha256-Zqvq0ArIWcH1jCcGeDJrc8X9aqSk0+xkgzFDZMZsEgY=";
   };
 
-  vendorHash = "sha256-rWcrCnq3sLWu1KaRJYjo9ym/HWj5iA5MpU/Y9E0V3No=";
+  vendorHash = "sha256-l9by0e3a4qTJxEUfTBo8MoWiJnUuZnkWC1sVZOlSy4A=";
 
   env.CGO_ENABLED = 0;
   ldflags = ["-X main.version=${finalAttrs.src.tag}"];
