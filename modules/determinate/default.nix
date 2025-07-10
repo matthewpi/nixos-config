deb01f5058c3c90ceabb3c92a364b267c72ebec0
Matthew Penner <me@matthewp.io>
2025-07-10 16:14:03 -0600

    determinate-nixd: install shell completions
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/determinate/default.nix
+++ modules/determinate/default.nix
@@ -5,6 +5,8 @@
     pkgs,
     ...
   }: let
+    cfg = config.determinate;
+
     # Stronger than mkDefault (1000), weaker than mkForce (50) and the "default override priority"
     # (100).
     mkPreferable = lib.mkOverride 750;
@@ -12,63 +14,55 @@
     # Stronger than the "default override priority", as the upstream module uses that, and weaker than mkForce (50).
     mkMorePreferable = lib.mkOverride 75;
 
-    determinate-nixd =
-      pkgs.runCommand "determinate-nixd" rec {
-        pname = "determinate-nixd";
-        version = "3.7.0";
-        name = "${pname}-${version}";
-        meta.mainProgram = "determinate-nixd";
-      } ''
-        mkdir -p "$out"/bin
-        cp ${inputs."determinate-nixd-${pkgs.stdenv.system}"} "$out"/bin/determinate-nixd
-        chmod +x "$out"/bin/determinate-nixd
-        "$out"/bin/determinate-nixd --help
-      '';
-
-    nix = inputs.nix.packages.${pkgs.stdenv.system}.nix.override (oldAttrs: {
-      # Disable nix-functional-tests.
-      nix-functional-tests = oldAttrs.nix-functional-tests.overrideAttrs {
-        doCheck = false;
-      };
-    });
+    determinatePackages = {
+      determinate-nix = inputs.nix.packages.${pkgs.stdenv.system}.nix.override (oldAttrs: {
+        # Disable nix-functional-tests.
+        nix-functional-tests = oldAttrs.nix-functional-tests.overrideAttrs {
+          doCheck = false;
+        };
+      });
+      determinate-nixd = pkgs.callPackage ./determinate-nixd.nix {inherit inputs;};
+    };
   in {
-    nix.package = nix;
-    environment.systemPackages = [determinate-nixd];
-
-    # NOTE(cole-h): Move the generated nix.conf to /etc/nix/nix.custom.conf, which is included from
-    # the Determinate Nixd-managed /etc/nix/nix.conf.
-    environment.etc."nix/nix.conf".target = "nix/nix.custom.conf";
-
-    systemd.services.nix-daemon.serviceConfig = {
-      ExecStart = [
-        ""
-        "@${lib.getExe determinate-nixd} determinate-nixd --nix-bin ${config.nix.package}/bin daemon"
-      ];
-      KillMode = mkPreferable "process";
-      LimitNOFILE = mkMorePreferable 1048576;
-      LimitSTACK = mkPreferable "64M";
-      TasksMax = mkPreferable 1048576;
+    options.determinate = {
+      enable = lib.mkEnableOption "Determinate Nix" // {default = true;};
+      package = lib.mkPackageOption determinatePackages "determinate-nix" {};
+      daemonPackage = lib.mkPackageOption determinatePackages "determinate-nixd" {};
     };
 
-    systemd.sockets.nix-daemon.socketConfig.FileDescriptorName = "nix-daemon.socket";
-    systemd.sockets.determinate-nixd = {
-      description = "Determinate Nixd Daemon Socket";
-      wantedBy = ["sockets.target"];
-      before = ["multi-user.target"];
+    config = lib.mkIf cfg.enable {
+      # Move the generated nix.conf to /etc/nix/nix.custom.conf, which is
+      # included from the Determinate Nixd-managed /etc/nix/nix.conf.
+      environment.etc."nix/nix.conf".target = "nix/nix.custom.conf";
 
-      unitConfig = {
-        RequiresMountsFor = ["/nix/store" "/nix/var/determinate"];
+      # Use the determinate-nix packages.
+      nix.package = cfg.package;
+      environment.systemPackages = [cfg.daemonPackage];
+
+      systemd.services.nix-daemon.serviceConfig = {
+        ExecStart = [
+          ""
+          "@${lib.getExe cfg.daemonPackage} determinate-nixd --nix-bin ${config.nix.package}/bin daemon"
+        ];
+        KillMode = mkPreferable "process";
+        LimitNOFILE = mkMorePreferable 1048576;
+        LimitSTACK = mkPreferable "64M";
+        TasksMax = mkPreferable 1048576;
       };
 
-      socketConfig = {
-        Service = "nix-daemon.service";
-        FileDescriptorName = "determinate-nixd.socket";
-        ListenStream = "/nix/var/determinate/determinate-nixd.socket";
-        DirectoryMode = "0755";
+      systemd.sockets.nix-daemon.socketConfig.FileDescriptorName = "nix-daemon.socket";
+      systemd.sockets.determinate-nixd = {
+        description = "Determinate Nixd Daemon Socket";
+        wantedBy = ["sockets.target"];
+        before = ["multi-user.target"];
+        unitConfig.RequiresMountsFor = ["/nix/store" "/nix/var/determinate"];
+        socketConfig = {
+          Service = "nix-daemon.service";
+          FileDescriptorName = "determinate-nixd.socket";
+          ListenStream = "/nix/var/determinate/determinate-nixd.socket";
+          DirectoryMode = "0755";
+        };
       };
     };
-
-    # Enable lazy-trees by default.
-    nix.settings.lazy-trees = lib.mkDefault false;
   };
 }
---
+++ modules/determinate/determinate-nixd.nix
@@ -0,0 +1,41 @@
+{
+  inputs,
+  installShellFiles,
+  lib,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "determinate-nixd";
+  version = "3.7.0";
+  src = inputs."determinate-nixd-${stdenvNoCC.system}";
+
+  dontUnpack = true;
+  dontBuild = true;
+
+  nativeBuildInputs = [installShellFiles];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out"/bin
+    install -Dm755 "$src" "$out"/bin/${finalAttrs.meta.mainProgram}
+
+    runHook postInstall
+  '';
+
+  postInstall = lib.optionalString (stdenvNoCC.buildPlatform.canExecute stdenvNoCC.hostPlatform) ''
+    installShellCompletion --cmd ${finalAttrs.meta.mainProgram} \
+      --bash <("$out"/bin/${finalAttrs.meta.mainProgram} completion bash) \
+      --fish <("$out"/bin/${finalAttrs.meta.mainProgram} completion fish) \
+      --zsh  <("$out"/bin/${finalAttrs.meta.mainProgram} completion zsh)
+  '';
+
+  meta = {
+    mainProgram = "determinate-nixd";
+    description = "Daemon that makes your experience of installing and using Nix dramatically more smooth";
+    homepage = "https://docs.determinate.systems/determinate-nix";
+    license = {fullName = "Unfree";};
+    sourceProvenance = with lib.sourceTypes; [binaryNativeCode];
+    platforms = ["x86_64-linux"];
+  };
+})
