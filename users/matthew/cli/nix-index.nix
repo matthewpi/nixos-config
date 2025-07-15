339187a94ba2fe2051038b2ddfa964198d0cf6a1
Matthew Penner <me@matthewp.io>
2025-07-15 00:02:09 -0600

    chore: fix `nix-index-database` rename
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/nix-index.nix
+++ users/matthew/cli/nix-index.nix
@@ -1,6 +1,6 @@
 {inputs, ...}: {
   imports = [
-    inputs.nix-index-database.hmModules.nix-index
+    inputs.nix-index-database.homeModules.nix-index
   ];
 
   programs.nix-index-database.comma.enable = true;
