fd111aaeba935895596dafe8881503b743abfbc3
Matthew Penner <me@matthewp.io>
2025-07-18 12:21:54 -0600

    freelens-k8s-proxy: 1.3.4 -> 1.3.6
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/freelens-k8s-proxy/package.nix
+++ packages/freelens-k8s-proxy/package.nix
@@ -5,16 +5,24 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "freelens-k8s-proxy";
-  version = "1.3.4";
+  version = "1.3.6";
 
   src = fetchFromGitHub {
     owner = "freelensapp";
     repo = "freelens-k8s-proxy";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Zqvq0ArIWcH1jCcGeDJrc8X9aqSk0+xkgzFDZMZsEgY=";
+    hash = "sha256-zgxNd0zImTKcATqc9MXLEEAmelxe6eRP36aZPNQPaFk=";
   };
 
-  vendorHash = "sha256-l9by0e3a4qTJxEUfTBo8MoWiJnUuZnkWC1sVZOlSy4A=";
+  vendorHash = "sha256-rnkmKFrAWuvln5zsXC3aaInbXyTLxAgVZab3pdFNT3U=";
+
+  # I love Go, except for the decision to pin the Go version in `go.mod`.
+  #
+  # If we are below the version specified in the `go.mod`, even by a single
+  # minor, the build will fail.
+  prePatch = ''
+    substituteInPlace go.mod --replace-fail 'go 1.24.5' 'go 1.24.4'
+  '';
 
   env.CGO_ENABLED = 0;
   ldflags = ["-X main.version=${finalAttrs.src.tag}"];
