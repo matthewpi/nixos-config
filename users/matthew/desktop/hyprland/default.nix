74ec24a0c4fedd5207da091884fa5d77adb15c0b
Matthew Penner <me@matthewp.io>
2025-07-20 12:54:59 -0600

    users(matthew): autostart 1Password silently
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/hyprland/default.nix
+++ users/matthew/desktop/hyprland/default.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  lib,
   nixosConfig,
   pkgs,
   ...
@@ -16,39 +17,42 @@
     ../../../../modules/ags
   ];
 
-  home.packages = with pkgs; [
-    # GNOME
-    bustle
-    celluloid
-    dconf-editor
-    file-roller
-    geary
-    gnome-calculator
-    gnome-calendar
-    gnome-characters
-    gnome-clocks
-    gnome-contacts
-    gnome-disk-utility
-    gnome-font-viewer
-    gnome-logs
-    gnome-maps
-    gnome-text-editor
-    gnome-weather
-    loupe
-    nautilus
-    papers
-    seahorse
-    showtime
-    sushi
+  home.packages =
+    (with pkgs; [
+      # GNOME
+      bustle
+      celluloid
+      dconf-editor
+      file-roller
+      geary
+      gnome-calculator
+      gnome-calendar
+      gnome-characters
+      gnome-clocks
+      gnome-contacts
+      gnome-disk-utility
+      gnome-font-viewer
+      gnome-logs
+      gnome-maps
+      gnome-text-editor
+      gnome-weather
+      loupe
+      nautilus
+      papers
+      seahorse
+      showtime
+      sushi
 
-    # clipboard
-    wl-clipboard
-
-    (makeAutostartItem {
+      # clipboard
+      wl-clipboard
+    ])
+    ++ lib.optional nixosConfig.programs._1password-gui.enable (pkgs.makeAutostartItem {
       name = "1password";
       package = nixosConfig.programs._1password-gui.package;
-    })
-  ];
+      # Do not open a window when started. This causes 1Password to only appear
+      # in the tray where it can then be unlocked later.
+      prependExtraArgs = ["--silent"];
+    });
 
   services.polkit = {
     enable = true;
