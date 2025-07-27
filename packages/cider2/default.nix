573e628e085a5df3a6479e39314661f194e72731
Matthew Penner <me@matthewp.io>
2025-07-27 14:13:36 -0600

    cider2: 2.6.1 -> 3.0.2
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/cider2/default.nix
+++ packages/cider2/default.nix
@@ -5,13 +5,13 @@
   stdenv,
 }: let
   pname = "cider2";
-  version = "2.6.1";
+  version = "3.0.2";
 
   # For anyone wondering, these files are locked behind firewall rules.
   # If you want Cider2, go buy it yourself.
-  sources. x86_64-linux = {
+  sources.x86_64-linux = {
     url = "https://r2.matthewp.io/cider/${version}/Cider-${version}.AppImage";
-    hash = "sha256-buHunUtFQZ14YNHngx3Hwqm5RonwE6C/SDh2xrTWUGI=";
+    hash = "sha256-XVBhMgSNJAYTRpx5GGroteeOx0APIzuHCbf+kINT2eU=";
   };
 
   src = fetchurl {
