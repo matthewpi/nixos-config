73d0d7cf2c6ae3232f034be95fd5fdda297354f1
Matthew Penner <me@matthewp.io>
2025-08-29 13:36:02 -0600

    users(matthew): fix ssh config
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/ssh.nix
+++ users/matthew/cli/ssh.nix
@@ -1,15 +1,25 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     # Include the 1Password bookmarks config, this also helps ensure we only
     # use the proper SSH key for the host.
     includes = ["~/.ssh/1Password/config"];
 
-    matchBlocks."*".extraOptions = {
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
       # Change the known_hosts location to outside `~/.ssh` as `~/.ssh` is not
       # persisted by impermanence.
-      UserKnownHostsFile = "~/.cache/ssh/known_hosts";
+      userKnownHostsFile = "~/.cache/ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
     };
   };
 }
