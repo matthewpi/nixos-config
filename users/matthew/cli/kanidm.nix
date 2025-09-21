ef32d065911b01e002ea577c60553ca6e2539156
Matthew Penner <me@matthewp.io>
2025-09-21 14:47:33 -0600

    chore: use version pinned kanidm package
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/kanidm.nix
+++ users/matthew/cli/kanidm.nix
@@ -4,17 +4,17 @@
       # Only add the kanidm binary to our path, not all the server ones.
       rec {
         name = "bin/kanidm";
-        path = "${pkgs.kanidm}/${name}";
+        path = "${pkgs.kanidm_1_7}/${name}";
       }
       # Bash completions
       rec {
         name = "share/bash-completion/kanidm.bash";
-        path = "${pkgs.kanidm}/${name}";
+        path = "${pkgs.kanidm_1_7}/${name}";
       }
       # ZSH completions
       rec {
         name = "share/zsh/site-functions/_kanidm";
-        path = "${pkgs.kanidm}/${name}";
+        path = "${pkgs.kanidm_1_7}/${name}";
       }
     ])
   ];
