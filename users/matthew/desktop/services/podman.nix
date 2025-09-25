7cde789b5e634ae569a2eeb47b5962acec7e5d7d
Matthew Penner <me@matthewp.io>
2025-09-25 15:54:46 -0600

    users(matthew): store podman credentials in secretservice
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/desktop/services/podman.nix
+++ users/matthew/desktop/services/podman.nix
@@ -1,26 +1,32 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }: let
   cfg = config.services.podman;
 in {
-  services.podman = {
-    enable = true;
+  services.podman.enable = true;
 
-    # Allow some insecure registries to be used.
-    # registries.insecure = lib.mkDefault ["127.0.0.1:8790" "localhost:8790"];
-  };
-
-  # TODO: PR into home-manager
-  xdg.configFile = lib.mkIf cfg.enable {
+  xdg.configFile = {
+    # TODO: PR into home-manager
     "systemd/user/podman.socket".source = "${cfg.package}/share/systemd/user/podman.socket";
     "systemd/user/podman.service".source = "${cfg.package}/share/systemd/user/podman.service";
     "systemd/user/sockets.target.wants/podman.socket".source = "${cfg.package}/share/systemd/user/podman.socket";
+
+    # Pre-configure auth to use the `secretservice` credential helper instead
+    # of storing credentials on disk in a plain-text.
+    "containers/auth.json".text = builtins.toJSON {
+      credHelpers = {
+        "docker.io" = "secretservice";
+        "ghcr.io" = "secretservice";
+        "quay.io" = "secretservice";
+        "zot.blahaj.systems" = "secretservice";
+      };
+    };
   };
 
   home.packages = [
+    pkgs.docker-credential-helpers
     pkgs.docker-compose
     pkgs.kind
   ];
@@ -31,5 +37,7 @@ in {
 
     # Set DOCKER_HOST to point to the rootless Podman socket.
     DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
+
+    REGISTRY_AUTH_FILE = "${config.home.sessionVariables.XDG_CONFIG_HOME}/containers/auth.json";
   };
 }
