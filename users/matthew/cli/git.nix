128f3236bb881987ac8de2504e11e9471ba7b67a
Matthew Penner <me@matthewp.io>
2025-10-23 13:47:46 -0600

    users(matthew): fix home-manager Git deprecations
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/git.nix
+++ users/matthew/cli/git.nix
@@ -2,28 +2,13 @@
   programs.git = {
     enable = true;
 
-    # User settings
-    userEmail = "me@matthewp.io";
-    userName = "Matthew Penner";
+    settings = {
+      # User details.
+      user = {
+        email = "me@matthewp.io";
+        name = "Matthew Penner";
+      };
 
-    # Enable Git LFS
-    lfs.enable = true;
-
-    # Ignore directories and files that should never be committed globally.
-    ignores = [".direnv" "result*"];
-
-    # Use the 1Password SSH Agent for commit signing.
-    signing = {
-      format = "ssh";
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKL873MsP1OFfffNC8n9WcVuOXOSW65/q26MIzib0K9k";
-      signByDefault = true;
-      signer =
-        if pkgs.stdenv.isDarwin
-        then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-        else "op-ssh-sign";
-    };
-
-    extraConfig = {
       # Disable advice.
       advice.detachedHead = false;
 
@@ -42,37 +27,53 @@
       # Write the commit graph persistently.
       feature.writeCommitGraph = true; # NOTE: this option seems to piss of Cargo when dealing with submodules
       fetch.writeCommitGraph = true; # NOTE: this option seems to piss of Cargo when dealing with submodules
-    };
 
-    difftastic = {
-      enable = true;
-      options = {
-        background = "dark";
-        color = "auto";
-        display = "side-by-side";
+      # Configure some aliases for commonly used commands and arguments.
+      aliases = {
+        a = "add";
+        aa = "add -A";
+
+        c = "commit";
+        ca = "commit --amend";
+        amend = "commit --amend --no-edit";
+
+        d = "diff";
+        dc = "diff --cached";
+
+        r = "rebase";
+
+        s = "stash";
+        sa = "stash apply";
+        sl = "stash list";
+        sp = "stash push -S";
+        ss = "stash show";
       };
     };
 
-    aliases = {
-      a = "add";
-      aa = "add -A";
+    # Enable Git LFS
+    lfs.enable = true;
 
-      c = "commit";
-      ca = "commit --amend";
-      amend = "commit --amend --no-edit";
+    # Ignore directories and files that should never be committed globally.
+    ignores = [".direnv" "result*"];
 
-      d = "diff";
-      dc = "diff --cached";
+    # Use the 1Password SSH Agent for commit signing.
+    signing = {
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKL873MsP1OFfffNC8n9WcVuOXOSW65/q26MIzib0K9k";
+      signByDefault = true;
+      signer =
+        if pkgs.stdenv.isDarwin
+        then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+        else "op-ssh-sign";
+    };
+  };
 
-      k = "kubectl";
-
-      r = "rebase";
-
-      s = "stash";
-      sa = "stash apply";
-      sl = "stash list";
-      sp = "stash push -S";
-      ss = "stash show";
+  programs.difftastic = {
+    enable = true;
+    options = {
+      background = "dark";
+      color = "auto";
+      display = "side-by-side";
     };
   };
 }
