cd093d5b61189091d4289fbde8ddfa179884442a
Matthew Penner <me@matthewp.io>
2025-12-06 21:00:35 -0700

    modules(hyprland): use `start-hyprland` command
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -39,7 +39,7 @@
       waylandCompositors.hyprland = {
         prettyName = "Hyprland";
         comment = "Hyprland compositor managed by UWSM";
-        binPath = "/run/current-system/sw/bin/Hyprland";
+        binPath = "/run/current-system/sw/bin/start-hyprland";
       };
     };
 
