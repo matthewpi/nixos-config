35618e9fd98845d88a3eb3b398e8acad7d16fdc1
Matthew Penner <me@matthewp.io>
2025-12-06 23:07:08 -0700

    packages: fix 1password and vesktop using Xwayland
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- packages/default.nix
+++ packages/default.nix
@@ -58,17 +58,28 @@
           "''${gappsWrapperArgs[@]}" \
           --suffix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
           --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [pkgs.udev]} \
-          --add-flags "\''${NIXOS_OZONE_WL:+--ozone-platform-hint=auto}"
+          --add-flags '--ozone-platform=wayland --ozone-platform-hint=auto'
       '';
     in (_packages
       // {
         _1password-gui = pkgs._1password-gui.overrideAttrs {preFixup = _1passwordPreFixup;};
         _1password-gui-beta = pkgs._1password-gui-beta.overrideAttrs {preFixup = _1passwordPreFixup;};
 
-        vesktop = pkgs.vesktop.override {
-          withTTS = false;
-          withSystemVencord = true;
-        };
+        vesktop =
+          (pkgs.vesktop.override {
+            withTTS = false;
+            withSystemVencord = true;
+          }).overrideAttrs {
+            postFixup =
+              lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+                makeWrapper ${lib.getExe pkgs.electron} "$out"/bin/vesktop \
+                  --add-flags "$out"/opt/Vesktop/resources/app.asar \
+                  --add-flags '--ozone-platform=wayland --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true'
+              ''
+              + lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+                makeWrapper "$out"/Applications/Vesktop.app/Contents/MacOS/Vesktop "$out"/bin/vesktop
+              '';
+          };
       });
   };
 }
