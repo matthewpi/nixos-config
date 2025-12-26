5b0392c0317f434f23834fec14a67a8e2d0a2664
Matthew Penner <me@matthewp.io>
2025-12-26 15:01:48 -0700

    modules(hyprland): cleanup gsettings
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- modules/hyprland/gsettings.nix
+++ modules/hyprland/gsettings.nix
@@ -1,56 +1,57 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  schemas =
+    pkgs.runCommand "gsettings-schemas" {
+      nativeBuildInputs = with pkgs; [glib];
+
+      # TODO: see if there is a better way to do this without needing to manually specify packages
+      # that have gsettings-schemas.
+      paths = with pkgs; [
+        gsettings-desktop-schemas
+        gtk3
+        gtk4
+
+        amberol
+        bustle
+        celluloid
+        dconf-editor
+        file-roller
+        geary
+        gnome-calculator
+        gnome-calendar
+        gnome-characters
+        gnome-clocks
+        gnome-contacts
+        gnome-disk-utility
+        gnome-font-viewer
+        gnome-logs
+        gnome-maps
+        gnome-text-editor
+        gnome-weather
+        loupe
+        nautilus
+        papers
+        seahorse
+        showtime
+        sushi
+      ];
+    } ''
+      mkdir schemas
+      # Get the gsettings-schemas for all paths
+      for path in $paths; do
+        cp "$path"'/share/gsettings-schemas/'*'/glib-2.0/schemas/'*.xml ./schemas || :
+        cp "$path"'/share/gsettings-schemas/'*'/glib-2.0/schemas/'*.override ./schemas || :
+      done
+
+      # Compile all the schemas
+      glib-compile-schemas ./schemas
+
+      mkdir -p "$out"/share/gsettings-schemas/glib-2.0
+      cp -r schemas "$out"/share/gsettings-schemas/glib-2.0/
+    '';
+in {
   # Ensure the proper gsettings-schemas are installed and properly accessible.
   environment.pathsToLink = ["/share/gsettings-schemas/glib-2.0"];
-  environment.systemPackages = [
-    (pkgs.runCommand "gsettings-schemas" {
-        nativeBuildInputs = with pkgs; [glib];
-
-        # TODO: see if there is a better way to do this without needing to manually specify packages
-        # that have gsettings-schemas.
-        paths = with pkgs; [
-          gsettings-desktop-schemas
-          gtk3
-          gtk4
-
-          amberol
-          bustle
-          celluloid
-          dconf-editor
-          file-roller
-          geary
-          gnome-calculator
-          gnome-calendar
-          gnome-characters
-          gnome-clocks
-          gnome-contacts
-          gnome-disk-utility
-          gnome-font-viewer
-          gnome-logs
-          gnome-maps
-          gnome-text-editor
-          gnome-weather
-          loupe
-          nautilus
-          papers
-          seahorse
-          showtime
-          sushi
-        ];
-      } ''
-        mkdir schemas
-        # Get the gsettings-schemas for all paths
-        for path in $paths; do
-          cp "$path"'/share/gsettings-schemas/'*'/glib-2.0/schemas/'*.xml ./schemas || :
-          cp "$path"'/share/gsettings-schemas/'*'/glib-2.0/schemas/'*.override ./schemas || :
-        done
-
-        # Compile all the schemas
-        glib-compile-schemas ./schemas
-
-        mkdir -p "$out"/share/gsettings-schemas/glib-2.0
-        cp -r schemas "$out"/share/gsettings-schemas/glib-2.0/
-      '')
-  ];
+  environment.systemPackages = [schemas];
 
   environment.sessionVariables = {
     XDG_DATA_DIRS = ["/run/current-system/sw/share/gsettings-schemas"];
