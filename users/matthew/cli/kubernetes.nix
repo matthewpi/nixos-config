7a9379be7cc9e13e306661b4e833bd7fb87df70b
Matthew Penner <me@matthewp.io>
2026-02-26 16:11:38 -0700

    users(matthew): add `kuberc` file
    Signed-off-by: Matthew Penner <me@matthewp.io>

--- users/matthew/cli/kubernetes.nix
+++ users/matthew/cli/kubernetes.nix
@@ -1,13 +1,51 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [kubectl kubelogin-oidc kubernetes-helm];
+  home.packages = with pkgs; [
+    kubectl
+    kubelogin-oidc
+    kubernetes-helm
+  ];
 
   # Change the default kubeconfig location.
   home.sessionVariables.KUBECONFIG = "${config.xdg.configHome}/kubernetes/config.yaml";
 
   # Enable k9s.
   programs.k9s.enable = true;
+
+  # Configure a `kuberc` configuration for kubectl.
+  home.sessionVariables.KUBERC = "${config.xdg.configHome}/kubernetes/kuberc.yaml";
+  home.file.".kube/kubrc".source = config.xdg.configFile."kubernetes/kuberc.yaml".source;
+  xdg.configFile."kubernetes/kuberc.yaml".source = (pkgs.formats.yaml {}).generate "kuberc.yaml" {
+    apiVersion = "kubectl.config.k8s.io/v1beta1";
+    kind = "Preference";
+
+    # Only allow certain credential plugins.
+    credentialPluginPolicy = "Allowlist";
+    credentialPluginAllowlist = [
+      {
+        name = lib.getExe pkgs.kubelogin-oidc;
+      }
+      {
+        name = "/etc/profiles/per-user/${config.home.username}/bin/kubectl";
+      }
+    ];
+
+    # Configure defaults for some commands.
+    defaults = [
+      # Use server-side apply by default.
+      {
+        command = "apply";
+        options = [
+          {
+            name = "server-side";
+            default = "true";
+          }
+        ];
+      }
+    ];
+  };
 }
